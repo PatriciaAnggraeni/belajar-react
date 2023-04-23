5253cc016032255558055666b49ea25d0ccc56b9
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-04-23 21:10:52 +0700

    section 2 - functional components

--- latihan-react/src/App.js
+++ latihan-react/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 // import file component di sini (file js)
 import { Intro } from './components/Intro';
+import Welcome from './components/Welcome';
 
 function App() {
     return (
         <div className="App">
             {/* panggil fungsi Intro di sini, tag berupa nama fungsi */}
             <Intro/>
+
+            {/* panggil fungsi Welcome di sini, tag berupa nama fungsi */}
+            <Welcome/>
         </div>
     );
 }
---
+++ latihan-react/src/components/Welcome.js
@@ -0,0 +1,10 @@
+// mengimport module React ke dalam file js (wajib)
+import React from 'react';
+
+// menggunakan functional komponen
+function Welcome() {
+    return <h1>Welocme Back, User!</h1>
+}
+
+// eksport fungsi agarbisa diimport di app.js
+export default Welcome;
