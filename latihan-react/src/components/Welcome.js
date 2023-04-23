c6d7e1c84f5949ac469a289193d2c15b56782022
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-04-23 21:15:35 +0700

    section 3 - class components

--- latihan-react/src/components/Welcome.js
+++ latihan-react/src/components/Welcome.js
@@ -1,9 +1,16 @@
 // mengimport module React ke dalam file js (wajib)
-import React from 'react';
+import React, { Component } from 'react';
 
 // menggunakan functional komponen
-function Welcome() {
+/* function Welcome() {
     return <h1>Welocme Back, User!</h1>
+} */
+
+// menggunakan class component
+class Welcome extends Component {
+    render() {
+        return <h1>Welcome Back, User!</h1>
+    }
 }
 
 // eksport fungsi agarbisa diimport di app.js
